0db0b4d898b29dae2a6e99a080d5d5021d52676c
Linlin Yin <linlinyin@outlook.com>
2019-02-20 23:35:41 -0600

    homework2 script

--- Assignment 2.sql
+++ Assignment 2.sql
@@ -9,17 +9,18 @@ group by gender;
 #Q3. employees were working for the dept number ‘d007’ between 1985-01-01and 1991-03-07
 select dept_no,count(distinct emp_no)
 from dept_emp
-where dept_no='d007' 
-and ((from_date between '1985-01-01'and'1991-03-07') or (to_date between '1985-01-01'and'1991-03-07'))
+where  dept_no='d007' 
+and from_date <='1991-03-07' and to_date >='1985-01-01'
 group by dept_no;
 
 # Q4 distinct employee numbers;Group the employees in terms of employee numbers and obtain the average salary for each group
 select count(distinct emp_no)
 from salaries;
 
-select emp_no, avg(salary), count(emp_no)
+select emp_no, avg(salary), count(emp_no) 
 from salaries
 group by emp_no;
+
 
 #Q5. employees are listed as either senior staff or staff
 select count(distinct emp_no)
